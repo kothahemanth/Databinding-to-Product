75a7a6b1c2c6ad13607e3377f34a5a08d5e6dfc6
Kotha Hemanth <hemanthkotha17@gmail.com>
2024-04-02 12:32:39 +0530

    main

--- webapp/index.js
+++ webapp/index.js
@@ -22,12 +22,6 @@ sap.ui.require([
 		sap.ui.getCore().setModel(oModel);
 
 		// Create a resource bundle for language specific texts
-		// configured supportedLocales represent the i18n files present:
-		// * "" - i18n/i18n.properties
-		// configured fallbackLocale should represent one of these files
-		// * "" - according to the fallback chain the root bundle is the last fallback.
-		//   Configuring it explicitly avoids side effects when additional resource files are added.
-		// @see https://sdk.openui5.org/topic/ec753bc539d748f689e3ac814e129563
 		var oResourceModel = new ResourceModel({
 			bundleName: "sap.ui.demo.db.i18n.i18n",
 			fallbackLocale: "",
